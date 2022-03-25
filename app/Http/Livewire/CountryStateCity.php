c9330b99b6dc731028505ef11c7b1382a9bbc0ef
TheMustacheCode <themustachecode@gmail.com>
2022-03-25 10:19:30 -0400

    Selects anidados sin select2

--- app/Http/Livewire/CountryStateCity.php
+++ app/Http/Livewire/CountryStateCity.php
@@ -8,16 +8,17 @@ use App\Models\Country;
 use App\Models\State;
 use App\Models\City;
 
+
 class CountryStateCity extends Component
 {
-
     public $countries;
     public $states;
     public $cities;
 
-    public $selectedCountry = NULL;
+    public $selectedCountry = '';
     public $selectedState = NULL;
     public $selectedCity = NULL;
+
 
     public function mount()
     {
@@ -26,19 +27,24 @@ class CountryStateCity extends Component
         $this->cities = collect();
     }
 
+
+    public function render()
+    {
+        return view('livewire.country-state-city');
+    }
+
     public function updatedSelectedCountry($country)
     {
+        sleep(2);
         $this->states = State::where('country_id', $country)->get();
         $this->selectedState = NULL;
     }
 
     public function updatedSelectedState($state)
     {
-        $this->cities = City::where('state_id', $state)->get();
-    }
-
-    public function render()
-    {
-        return view('livewire.country-state-city');
+        if (!is_null($state)) {
+            $this->cities = City::where('state_id', $state)->get();
+        }
     }
 }
+
--- resources/views/livewire/country-state-city.blade.php
+++ resources/views/livewire/country-state-city.blade.php
@@ -1,71 +1,59 @@
 <div>
-    <form class="flex flex-col space-y-4">
-        @csrf
-        <div wire:ignore>
-            <label for="" class="text-sm">País Select 2</label>
-            <select 
-                wire:model="selectedCountry"
-                id="select2-country">
-                <option value="" selected>Seleccione un país</option>
-                @foreach($countries as $country)
-                    <option value="{{ $country->id }}">{{ $country->name }}</option>
-                @endforeach
-            </select>
-            @error('country')
-            <p class="text-red-500 text-sm">{{ $message }}</p>
-            @enderror
-        </div>
-
-        @if($selectedCountry)
-        <div>
-            <label for="" class="text-sm">Estado</label>
-            <select 
-                wire:model="selectedState"
-                id="state_id"
-                class="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none
-                focus:ring-2 focus:ring-purple-300 @error('state')border border-red-500 @enderror">
-                <option value="" selected>Debe seleccionar un estado</option>
+    <div>
+        <label for="" class="text-sm">País</label>
+        <select 
+            wire:model="selectedCountry"
+            id="country_id"
+            class="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none
+            focus:ring-2 focus:ring-purple-300">
+            <option value="" disabled selected>Seleccione un país</option>
+            @foreach($countries as $country)
+                <option value="{{ $country->id }}">{{ $country->name }}</option>
+            @endforeach
+        </select>
+    </div>
+    @if($selectedCountry)
+    <div class="mt-2">
+        <label for="" class="text-sm">Estado </label>
+        <svg wire:target="selectedCountry" wire:loading class="animate-spin ml-1 h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+        <select 
+            wire:model="selectedState"
+            id="state_id"
+            class="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none
+            focus:ring-2 focus:ring-purple-300">
+            @if (!is_null($selectedCountry))
                 @foreach($states as $state)
                     <option value="{{ $state->id }}">{{ $state->name }}</option>
                 @endforeach
-            </select>
-            @error('state')
-            <p class="text-red-500 text-sm">{{ $message }}</p>
-            @enderror
-        </div>
-        @endif
-        @if($selectedState)
-        <div>
-            <label for="" class="text-sm">Ciudad</label>
-            <select 
-                wire:model="selectedCity"
-                id="city_id"
-                class="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none
-                focus:ring-2 focus:ring-purple-300 @error('city')border border-red-500 @enderror">
-                <option value="" selected>Debe seleccionar una ciudad</option>
+            @else
+            <option value="" disabled selected>Debe seleccionar un país</option>
+            @endif
+        </select>
+    </div>
+    @endif
+    @if($selectedState)
+    <div class="mt-2">
+        <label for="" class="text-sm">Ciudad</label>
+        <svg wire:target="selectedState" wire:loading class="animate-spin ml-1 h-5 w-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
+            <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
+            <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
+        </svg>
+        <select 
+            wire:model="selectedCity"
+            id="city_id"
+            class="ring-1 ring-gray-300 w-full rounded-md px-4 py-2 mt-2 outline-none
+            focus:ring-2 focus:ring-purple-300">
+            @if (!is_null($selectedState))
                 @foreach($cities as $city)
                     <option value="{{ $city->id }}">{{ $city->name }}</option>
                 @endforeach
-            </select>
-            @error('city')
-            <p class="text-red-500 text-sm">{{ $message }}</p>
-            @enderror
-        </div>
-        @endif
-        <button class="inline-flex self-end bg-purple-700 font-bold rounded-lg px-6 py-2 text-white disabled:opacity-50">
-            Guardar Dirección
-        </button>
-    </form>
+            @else
+            <option value="" disabled selected>Debe seleccionar un estado</option>
+            @endif
+        </select>
+    </div>
+    @endif
 </div>
-
-
-<script>
-document.addEventListener('DOMContentLoaded', function() {
-    $("#select2-country").select2();
-
-    $('#select2-country').on('change', function(e) {
-        var countryId = $('#select2-country').select2("val");
-        @this.set('selectedCountry', countryId);
-    });
-});
-</script>
